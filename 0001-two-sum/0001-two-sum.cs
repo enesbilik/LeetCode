152ad8b627d3327e8992201c9bb99aa0f3f6e217
Enes <60428493+enesbilik@users.noreply.github.com>
2023-08-20 00:27:45 +0300

    Time: 128 ms (96.82%), Space: 44.5 MB (39.13%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> numIndices = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            if (numIndices.ContainsKey(complement))
+            {
+                return new int[] { numIndices[complement], i };
+            }
+            numIndices[nums[i]] = i;
+        }
+
+        return null; 
+    }
+}
