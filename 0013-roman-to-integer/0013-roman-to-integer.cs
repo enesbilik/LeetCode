68311ba7ea207e0868ba64ec6283f68ab4da4cd8
Enes <60428493+enesbilik@users.noreply.github.com>
2023-08-20 13:28:16 +0300

    Time: 72 ms (66.02%), Space: 46.6 MB (87.64%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cs
+++ 0013-roman-to-integer/0013-roman-to-integer.cs
@@ -1,45 +1,29 @@
 public class Solution {
     public int RomanToInt(string s)
-{
-    int answer = 0, number = 0, prev = 0;
-
-    for (int j = s.Length - 1; j >= 0; j--)
     {
-        switch (s[j])
+        int answer = 0, number = 0, prev = 0;
+        for (var j = s.Length - 1; j >= 0; j--)
         {
-            case 'M':
-                number = 1000;
-                break;
-            case 'D':
-                number = 500;
-                break;
-            case 'C':
-                number = 100;
-                break;
-            case 'L':
-                number = 50;
-                break;
-            case 'X':
-                number = 10;
-                break;
-            case 'V':
-                number = 5;
-                break;
-            case 'I':
-                number = 1;
-                break;
-        }
-        if (number < prev)
-        {
-            answer -= number;
-        }
-        else
-        {
-            answer += number;
-        }
-        prev = number;
-    }
-    return answer;
-}
+            number = s[j] switch
+            {
+                'M' => 1000,
+                'D' => 500,
+                'C' => 100,
+                'L' => 50,
+                'X' => 10,
+                'V' => 5,
+                'I' => 1,
+                _ => number
+            };
 
+            if (number < prev)
+                answer -= number;
+            else
+                answer += number;
+            prev = number;
+        }
+
+        Console.WriteLine(answer);
+        return answer;
+    }
 }
