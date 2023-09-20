4bb84f8eff45267665ccae6bb249fcf6f513f415
Enes <60428493+enesbilik@users.noreply.github.com>
2023-09-20 22:27:31 +0300

    Time: 62 ms (78.49%), Space: 37.9 MB (29.03%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cs
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cs
@@ -1,9 +1,7 @@
 public class Solution {
     public bool SquareIsWhite(string coordinates)
     {
-        var letter = (int)coordinates[0] - 97;
-        var number =  coordinates[1] - '0' -1 ;
-
-        return (letter + number) % 2 != 0;
+        return coordinates.Sum(value => (int)value) % 2 != 0;
+        
     }
 }
